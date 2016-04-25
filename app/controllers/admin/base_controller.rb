903eda093dd54797b810bee9c73f8196f6fa9394
Marian <marianhalliday@gmail.com>
2016-04-25 09:56:18 -0400

    Adding authentication to admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,4 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
+  http_basic_authenticate_with name: ENV.fetch('AUTH_USER', 'user'), password: ENV.fetch('AUTH_PASSWORD', 'password')
 end
