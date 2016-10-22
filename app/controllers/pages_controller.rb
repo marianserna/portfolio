ad44394e39cdecf17f0034d26d598fe716593567
Marian <marianhalliday@gmail.com>
2016-10-21 22:42:05 -0400

    Styling changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @portfolio_items = PortfolioItem.where.not(category: 'photography').order(position: :desc).all
-    @photographs = PortfolioItem.where(category: 'photography').order(position: :desc).all
+    @portfolio_items = PortfolioItem.order(position: :desc).all
   end
 end
