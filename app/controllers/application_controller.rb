684481e1a0c7133737146b2d63aef2917cf3c955
Marian <marianhalliday@gmail.com>
2017-04-10 20:39:28 -0400

    Setting links to be https by default on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  def default_url_options(options = {})
+    options.merge!(protocol: :https) if Rails.env.production?
+    options
+  end
 end
