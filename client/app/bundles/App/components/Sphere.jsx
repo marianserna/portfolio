4567fe52085d08f2be63bb29ef17005b94dcbc88
Marian Serna <marianhalliday@gmail.com>
2018-04-02 07:15:06 -0400

    Landing change

--- client/app/bundles/App/components/Sphere.jsx
+++ client/app/bundles/App/components/Sphere.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import SphereScene from './SphereScene'
-import {TweenMax, TimelineLite} from 'gsap';
+import SphereScene from './SphereScene';
+import { TweenMax, TimelineLite } from 'gsap';
 import './DrawSVGPlugin';
 import SplitText from './SplitText';
 import FontAwesome from 'react-fontawesome';
-import {Howl} from 'howler';
+import { Howl } from 'howler';
 
 export default class Sphere extends React.Component {
   constructor(props, _railsContext) {
@@ -14,21 +14,26 @@ export default class Sphere extends React.Component {
   }
 
   componentDidMount() {
-    this.sphere = new SphereScene(
-      this.sphereContainer
-    );
+    this.sphere = new SphereScene(this.sphereContainer);
 
-    TweenMax.fromTo('h1', 1,
-      {css: {y: 125}},
-      {css: {y: 0}, delay: 0.4, ease: Power4.easeOut, onComplete: () => {
-        document.querySelector('h3').classList.remove('hidden');
+    TweenMax.fromTo(
+      'h1',
+      1,
+      { css: { y: 125 } },
+      {
+        css: { y: 0 },
+        delay: 0.4,
+        ease: Power4.easeOut,
+        onComplete: () => {
+          document.querySelector('h3').classList.remove('hidden');
 
-        const tl = new TimelineLite();
-        const title = new SplitText("h3", {type: "words,chars"});
-        const chars = title.chars;
+          const tl = new TimelineLite();
+          const title = new SplitText('h3', { type: 'words,chars' });
+          const chars = title.chars;
 
-        tl.staggerFromTo(chars, 0.3, {opacity: 0}, {opacity: 1}, 0.05);
-      }}
+          tl.staggerFromTo(chars, 0.3, { opacity: 0 }, { opacity: 1 }, 0.05);
+        }
+      }
     );
 
     this.song.play();
@@ -42,34 +47,33 @@ export default class Sphere extends React.Component {
   }
 
   render() {
-    return(
+    return (
       <div id="landingSphere">
-        <div id="sphere" ref={(div) => this.sphereContainer = div}></div>
+        <div id="sphere" ref={div => (this.sphereContainer = div)} />
         <span className="name">
           <h1>Marian Serna</h1>
         </span>
-        <h3 className="title hidden">full stack - interactive developer</h3>
+        <h3 className="title hidden">Developer</h3>
 
         <div className="social">
-          <a className="github" href="https://github.com/marianserna?tab=repositories" target="_blank">
-            <FontAwesome
-              className='fa-icon'
-              name='github'
-              size='2x'
-            />
+          <a
+            className="github"
+            href="https://github.com/marianserna?tab=repositories"
+            target="_blank"
+          >
+            <FontAwesome className="fa-icon" name="github" size="2x" />
             GITHUB
           </a>
-          <a className="linkedin" href="https://www.linkedin.com/in/marian-serna-1762337b/" target="_blank">
-            <FontAwesome
-              className='fa-icon'
-              name='linkedin'
-              size='2x'
-            />
+          <a
+            className="linkedin"
+            href="https://www.linkedin.com/in/marian-serna-1762337b/"
+            target="_blank"
+          >
+            <FontAwesome className="fa-icon" name="linkedin" size="2x" />
             LINKEDIN
           </a>
         </div>
       </div>
-
-    )
+    );
   }
 }
